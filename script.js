e36e54cf0317e8292306c20aa5e04a780de801f5
jdlopez-dev <72524993+jdlopez-dev@users.noreply.github.com>
2020-12-31 13:30:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var today = new Date();
+
+document.getElementById("fecha").innerHTML = today.toDateString();
+
+today.setDate(today.getDate()+2);
+
+document.getElementById("fecha_02").innerHTML = today.toDateString();
+
+var year = today.getFullYear().toString();
+document.getElementById("year").innerHTML = year;
